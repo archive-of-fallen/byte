bdb896f0bdd01b66a3a7c9074ad10c7810a2bf58
FallenLight (illumi) <xFallenLight@users.noreply.github.com>
2017-12-10 21:41:52 +0800

    Remove some stuff.

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ client.on('ready', () => {
 
 let prefix = "b:"
 
-let blacklistedIds = [ "229552088525438977", "260246864979296256" ]
+let blacklistedIds = [ "229552088525438977" ]
 
 client.on('message', msg => {
     if (!msg.content.startsWith(prefix)) return;
     if (msg.author.bot) return;
-    if (blacklistedIds.includes(msg.author.id)) return msg.reply("Test");
+    if (blacklistedIds.includes(msg.author.id)) return;
   
     if (msg.content.startsWith(prefix + 'ping')) {
         msg.channel.send("Pinging...").then(sent => {
