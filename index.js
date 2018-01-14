7a1d123b32aa08fe57babeaa6cbb0348d83c379f
FallenLight <xFallenLight@users.noreply.github.com>
2018-01-14 10:09:16 +0800

    Add contributor.

--- index.js
+++ index.js
@@ -128,7 +128,7 @@ client.on('message', msg => {
       .addField(`Full Command Reference`, `Type b:cmds`)
       .addField(`Invite`, `[here](https://discordapp.com/oauth2/authorize/?permissions=8&scope=bot&client_id=388630510399782912)`)
       .addField(`Support Server`, `[here](https://discord.gg/2AxaHpf)`)
-      .addField(`Contributor List`, `None!`)
+      .addField(`Contributor List`, `Puerosola#5543`)
       .addField(`Author`, `FallenLight#5149`)
       // .setFooter(``)
       
