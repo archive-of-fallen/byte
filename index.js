45181508c0c0d7de18db66084a2cb24741bd8848
FallenLight <xFallenLight@users.noreply.github.com>
2018-04-10 17:22:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -184,12 +184,13 @@ client.on('message', msg => {
     }
   
     if (msg.content.startsWith(prefix + 'members')) {
-      msg.channel.send(`Total members: ${msg.guild.memberCount}\n\n
+      msg.channel.send(`__**Inaccurate Statistics:**__
+\nTotal members: ${msg.guild.memberCount}\n\n
 __**More Indepth Statistics:**__
 \nBots: ${msg.guild.members.filter(mem => mem.user.bot).size}
-\nBot Listing:\n\`\`\`${msg.guild.members.filter(mem => mem.user.bot).map(bots => bots.user.tag).join('\n').toString()}\`\`\`
+\nBot Listing:\`\`\`${msg.guild.members.filter(mem => mem.user.bot).map(bots => bots.user.tag).join(',\n').toString()}\`\`\`
 \n\nUsers: ${msg.guild.members.filter(mem => !mem.user.bot).size}
-\nUser Listing:\n\`\`\`${msg.guild.members.filter(mem => !mem.user.bot).map(users => users.user.tag).join('\n').toString()}\`\`\``);
+\nUser Listing:\`\`\`${msg.guild.members.filter(mem => !mem.user.bot).map(users => users.user.tag).join(',\n').toString()}\`\`\``);
     }
 
     if (msg.content.startsWith(prefix + 'cmds')) {
